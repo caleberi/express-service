141379e51d1e53e413ab9e5c275a72637fd358d7
Caleb Adewole <caleberioluwa@gmail.com>
2021-04-29 22:14:10 +0100

    fixed validation message

--- app/helpers/feedback.validator.js
+++ app/helpers/feedback.validator.js
@@ -5,11 +5,16 @@ async function formChecker(req, res, next) {
   await check('email')
     .isEmail()
     .normalizeEmail()
-    .withMessage('must be at least 5 chars long')
+    .withMessage('Invalid email address . Guy 😂 you are funny 😜!')
     .run(req);
-  await check('name').not().isEmpty().trim().run(req);
-  await check('title').not().isEmpty().trim().run(req);
-  await check('message').not().isEmpty().trim().run(req);
+  await check('name').not().isEmpty().trim().withMessage('is your name empty🤣?').run(req);
+  await check('title')
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage('you are making me laugh 😊😁😜🤣🤣')
+    .run(req);
+  await check('message').not().isEmpty().trim().withMessage('please include message').run(req);
   return req;
 }
 
--- app/routes/feedback.route.js
+++ app/routes/feedback.route.js
@@ -22,19 +22,14 @@ module.exports = (feedbackService) => {
     const errors = validationResult(req);
     const { name, email, title, message } = req.body;
     if (!errors.isEmpty()) {
-      res.status(401).json({
+      res.status(400).json({
         errors: errors.array(),
       });
       return;
     } else {
       res.status(200).json({
         errors: null,
-        feedback: {
-          name,
-          email,
-          title,
-          message,
-        },
+        feedback: { name, email, title, message },
       });
       return;
     }
