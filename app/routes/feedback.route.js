1b3778eff2daeae47163f1d7804912aa1f8fa126
Caleb Adewole <caleberioluwa@gmail.com>
2021-04-29 15:40:18 +0100

    fixing validation on form

--- app/routes/feedback.route.js
+++ app/routes/feedback.route.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const router = express.Router();
-
+const { check, validationResult } = require('express-validator');
 module.exports = (feedbackService) => {
   // eslint-disable-next-line no-unused-vars
   router.get('/', async function (req, res, next) {
@@ -14,9 +15,32 @@ module.exports = (feedbackService) => {
     return;
   });
   // eslint-disable-next-line no-unused-vars
-  router.post('/', function (req, res, next) {
-    res.send(`FeedBack Posted`);
-    return;
-  });
+  router.post(
+    '/api',
+    check('email').normalizeEmail().isEmail().withMessage('must be at least 5 chars long'),
+    check('name').isString().trim(),
+    check('title').isString().trim(),
+    !check('message').isEmpty(),
+    function (req, res, next) {
+      const errors = validationResult(req);
+      const { name, email, title, message } = req;
+      if (!errors.isEmpty()) {
+        res.status(401).json({
+          errors: errors.array(),
+        });
+        return;
+      }
+      res.json({
+        errors: null,
+        feedback: {
+          name,
+          email,
+          title,
+          message,
+        },
+      });
+      return;
+    }
+  );
   return router;
 };
